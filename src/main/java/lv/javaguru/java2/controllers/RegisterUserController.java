620b85a7bc934c7eddc9f4052468171151c23371
Kolert <kolert51@gmail.com>
2018-03-04 16:22:19 +0200

    user registration added

--- src/main/java/lv/javaguru/java2/controllers/RegisterUserController.java
+++ src/main/java/lv/javaguru/java2/controllers/RegisterUserController.java
@@ -2,13 +2,19 @@ package lv.javaguru.java2.controllers;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+@Scope("session")
 @Controller
 public class RegisterUserController {
 
@@ -19,7 +25,33 @@ public class RegisterUserController {
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
     public String registeUser(HttpServletRequest request,Model model) {
         model.addAttribute("h1Text","Text from controller");
-        return "user/registration";
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        String repPassword = request.getParameter("repPassword");
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        String email = request.getParameter("email");
+        Map<String,String> userMap = new HashMap<>();
+        userMap.put("login",login);
+        userMap.put("password",password);
+        userMap.put("repPassword",repPassword);
+        userMap.put("name",name);
+        userMap.put("surname",surname);
+        userMap.put("email",email);
+
+        List<Map<String,String>> users = (List<Map<String,String>>) request.getSession().getAttribute("users");
+        if(users!=null){
+            users.add(userMap);
+        }else{
+            users = new ArrayList<>();
+            users.add(userMap);
+            request.getSession().setAttribute("users",users);
+        }
+        //TODO createUser
+
+        //TODO getUser
+        model.addAttribute("users",users);
+        return "user/userList";
     }
 }
 
