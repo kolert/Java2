38c78151a50b8ef0c7b4f9ff275f51aae043c46c
murz1 <murz1k@inbox.lv>
2018-04-25 19:47:42 +0300

    Supplies page added

---
+++ src/main/java/lv/javaguru/java2/controllers/SuppliesController.java
@@ -0,0 +1,22 @@
+package lv.javaguru.java2.controllers;
+
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@Scope("session")
+public class SuppliesController {
+
+    @RequestMapping(value = "/supplies", method = RequestMethod.GET)
+    public String home(HttpServletRequest request, HttpSession session, Model model) {
+        return "user/supplies";
+    }
+}
+
